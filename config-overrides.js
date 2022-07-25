830c2b3fcba5a988b81588f87157f2829a9b9563
legiorex <batyushev@gmail.com>
2022-07-25 21:56:18 +0300

    first-commit

---
+++ config-overrides.js
@@ -0,0 +1,22 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path')
+
+module.exports = function override(config) {
+    return {
+        ...config,
+        resolve: {
+            ...config.resolve,
+            alias: {
+                ...config.alias,
+                '@components': path.resolve(__dirname, 'src/components'),
+                '@pages': path.resolve(__dirname, 'src/pages'),
+                '@styles': path.resolve(__dirname, 'src/styles'),
+                '@assets': path.resolve(__dirname, 'src/assets'),
+                '@store': path.resolve(__dirname, 'src/store'),
+                '@hooks': path.resolve(__dirname, 'src/hooks'),
+                '@utils': path.resolve(__dirname, 'src/utils'),
+                '@interfaces': path.resolve(__dirname, 'src/interfaces'),
+            },
+        },
+    }
+}
